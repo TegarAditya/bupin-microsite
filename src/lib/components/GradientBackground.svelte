40bcce30a4136c6a1dac32ea936b9bf4cce9f48d
Tegar Aditya <tegaraditya53@gmail.com>
2025-05-10 11:09:20 +0700

    feat: add GradientBackground component with SVG background

---
+++ src/lib/components/GradientBackground.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Background from '$lib/images/background.svg?inline';
+</script>
+
+<div class="fixed w-full h-screen -z-10 left-0 top-0">
+    <div class="absolute inset-0">
+        <img src={Background} alt="background" class="w-full h-full object-cover" />
+    </div>
+</div>
