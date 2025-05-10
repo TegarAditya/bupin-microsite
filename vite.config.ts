1e0fa533f6422d8998bf267302356db7bb8b61ff
Tegar Aditya <tegaraditya53@gmail.com>
2025-05-10 11:08:44 +0700

    add devtoolsJson plugin to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import tailwindcss from '@tailwindcss/vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [enhancedImages(), sveltekit(), tailwindcss()]
+	plugins: [devtoolsJson(), enhancedImages(), sveltekit(), tailwindcss()]
 });
