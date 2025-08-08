66c6c33158dc84ccb0ecf5d68444eb3df5837f82
Tegar Aditya <tegaraditya53@gmail.com>
2025-08-08 13:11:40 +0700

    fix: change color for visibility

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -44,8 +44,6 @@
 			icon: faDesktop
 		}
 	];
-
-	const thisYear = new Date().getFullYear();
 </script>
 
 <div class="flex flex-col items-center justify-center space-y-3.5">
@@ -56,27 +54,25 @@
 		fetchpriority="high"
 		height="500"
 		width="500"
-		class="mb-4 h-52 w-auto md:h-72"
+		class="mb-2 mt-5 h-52 w-auto md:h-72"
 	/>
-	<h1 class="text-center text-xl font-semibold md:text-2xl">Selamat Datang di Portal Bupin</h1>
-	<p class="text-sm text-gray-400 md:text-base">
+	<h1 class="text-center text-xl font-semibold md:text-2xl text-gray-800">
+		Selamat Datang di Portal Bupin
+	</h1>
+	<p class="text-sm text-gray-600 md:text-base">
 		Email: <a href="mailto:support@bupin.id" class="underline">support@bupin.id</a>
 	</p>
 </div>
 
 <div class="mt-6 w-full max-w-md space-y-4">
-	{#each links as { text, href, icon }, i}
+	{#each links as { text, href, icon }}
 		<a
 			{href}
 			target="_blank"
-			class="flex items-center justify-center rounded-lg bg-gray-800 px-6 py-3 text-center text-sm text-white transition hover:bg-gray-700 md:text-base"
+			class="flex items-center justify-center rounded-lg bg-white shadow-md px-6 py-3 text-center text-sm text-gray-800 transition hover:bg-gray-100 md:text-base"
 		>
 			<Fa {icon} class="mr-2 h-5 w-5" />
 			{text}
 		</a>
 	{/each}
-</div>
-
-<div class="mt-10 text-xs text-gray-400 md:absolute md:bottom-[15px] md:mt-0">
-	&copy; {thisYear} PT Margo Mitro Joyo. All rights reserved.
 </div>
