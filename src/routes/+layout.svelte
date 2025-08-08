7d1b751088af5d18eb60ae17d373c309a1bde849
Tegar Aditya <tegaraditya53@gmail.com>
2025-08-08 19:33:22 +0700

    fix: preload fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,10 @@
 	import '@fontsource/bricolage-grotesque/600.css';
 	import '@fontsource/inter/400.css';
 	import '@fontsource/inter/600.css';
+	import BricolageGrotesque400 from '@fontsource/bricolage-grotesque/files/bricolage-grotesque-latin-400-normal.woff2?url'
+	import BricolageGrotesque600 from '@fontsource/bricolage-grotesque/files/bricolage-grotesque-latin-600-normal.woff2?url'
+	import Inter400 from '@fontsource/inter/files/inter-latin-400-normal.woff2?url'
+	import Inter600 from '@fontsource/inter/files/inter-latin-600-normal.woff2?url'
 
 	let { children } = $props();
 
@@ -16,13 +20,17 @@
 	<meta http-equiv="x-ua-compatible" content="IE=7" />
 	<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 	<meta name="description" content="Akses berbagai aplikasi Bupin 4.0" />
+	<link rel="preload" as="font" type="font/woff2" href={BricolageGrotesque400} crossorigin="anonymous" />
+	<link rel="preload" as="font" type="font/woff2" href={BricolageGrotesque600} crossorigin="anonymous" />
+	<link rel="preload" as="font" type="font/woff2" href={Inter400} crossorigin="anonymous" />
+	<link rel="preload" as="font" type="font/woff2" href={Inter600} crossorigin="anonymous" />
 	<title>Portal Bupin</title>
 </svelte:head>
 
-<div class="relative flex min-h-screen flex-col items-center justify-between p-6 text-gray-800 bg-gradient-to-br from-pink-50 via-blue-50 to-purple-50">
+<div class="relative min-h-screen flex flex-col items-center justify-between px-6 text-gray-800 bg-gradient-to-br from-pink-50 via-blue-50 to-purple-50">
 	<div class="absolute -z-10 inset-0 bg-[radial-gradient(ellipse_at_center,rgba(255,255,255,0.6)_0%,transparent_70%)] pointer-events-none"></div>
 	{@render children()}
-	<div class="mt-10 mb-2 text-xs text-gray-500 md:absolute md:bottom-[15px]">
-		&copy; {thisYear} PT Margo Mitro Joyo. All rights reserved.
+	<div class="mt-10 mb-7 text-sm text-gray-500">
+		&copy; {thisYear} PT Margo Mitro Joyo.
 	</div>
 </div>
